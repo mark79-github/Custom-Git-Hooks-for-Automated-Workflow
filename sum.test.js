e5e8f81ce282561b57b7b13fd0e456725dd8ba25
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-07-23 08:45:24 +0300

    Add sum.js, sum.test.js

---
+++ sum.test.js
@@ -0,0 +1,9 @@
+const sum = require('./sum');
+
+// test('adds 1 + 2 to equal 5', () => {
+//     expect(sum(1, 2)).toBe(5);
+// });
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
