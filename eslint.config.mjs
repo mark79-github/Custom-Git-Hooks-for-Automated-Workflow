84d210b4769ef653d7db6eeaf5f0ddc2ac7055c7
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-07-23 08:49:24 +0300

    Add js files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,21 @@
+import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
+const { node } = globals;
 
 export default defineConfig([
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" },
+  },
+  {
+    files: ["**/*.{js}"],
+    languageOptions: { globals: node },
+  },
 ]);
